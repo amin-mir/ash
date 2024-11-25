625e72f306be9cc0890961b6283454df74121396
Amin Mir <aminmir326@gmail.com>
2024-11-25 08:16:38 +0200

    Make implementation async-signal-safe

--- 826.c
+++ 826.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <errno.h>
 #include <signal.h>
 #include <stdio.h>
@@ -43,9 +44,12 @@ typedef struct {
 static int next_jid = 0;
 static job jobs[MAXJOBS];
 
-// TODO: Reap children and update the job status to terminated.
-
 // Returns -1 when there are no empty slots to add the new job.
+// addjob is used in two scenarios:
+// 1. When adding a new job.
+// 2. When resuming a suspended job and running it in the background.
+// TODO: remove updating the status from this function and use setjobstatus
+// instead.
 int addjob(pid_t pid, Status st) {
   job *free_job;
   for (int i = 0; i < MAXJOBS; i++) {
@@ -75,6 +79,33 @@ int addjob(pid_t pid, Status st) {
   return -1;
 }
 
+// Returns jid when there is a match for the given pid, or -1 otherwise.
+int setjobstat(pid_t pid, Status st) {
+  for (int i = 0; i < MAXJOBS; i++) {
+    job *j = &jobs[i];
+
+    // An entry already exists for this pid;
+    if (j->pid == pid) {
+      j->st = st;
+      return j->jid;
+    }
+  }
+  return -1;
+}
+
+job *findjob(pid_t pid) {
+  for (int i = 0; i < MAXJOBS; i++) {
+    job *j = &jobs[i];
+
+    // An entry already exists for this pid;
+    // TODO: should it ignore terminated jobs?
+    if (j->pid == pid) {
+      return j;
+    }
+  }
+  return NULL;
+}
+
 char *jstatus_str(Status st) {
   switch (st) {
   case UNINIT:
@@ -101,33 +132,70 @@ void unix_error(char *msg) {
   exit(0);
 }
 
+// handles SIGINT and SIGTSTP.
 void forward_signal(int sig) {
+  // No need to save and restore errno as we don't expected any errors here.
+  sigset_t mask_all, prev_all;
+  if (sigfillset(&mask_all) < 0)
+    unix_error("sigfillset error");
+
+  if (sigprocmask(SIG_BLOCK, &mask_all, &prev_all) < 0)
+    unix_error("sigprocmask block error");
+
   // Only forward if there is a foreground process, otherwise
   // ignore the signal.
   if (fg_pid) {
-    printf("forwarding sig %d to %d\n", sig, fg_pid);
+    printf("[ForwardSignalHandler] forwarding sig %d to %d\n", sig, fg_pid);
+    /* It's tempting to try to handle the error and not fail in case
+     * of ESRCH, but we're making sure that we will only call kill for
+     * valid pids by checking fg_pid, and we want such errors to be deteced.
+     * Addtionally all signals are blocked when this function starts, so it
+     * eliminates the possibility of receiving SIGINT while handling SIGTSTP,
+     * thus we won't end up sending the same signal twice to a process.
+     * Due to implicit signal blocking, receiving the same signal won't
+     * interrupt handling another, even without an explicit sigprocmask. */
     if (kill(fg_pid, sig) < 0) {
-      unix_error("Forward signal error");
+      unix_error("Forward SIGINT|SIGTSTP error");
     }
-
-    Status st;
-    if (sig == SIGINT) {
-      st = TERMINATED;
-    } else {
-      st = STOPPED;
-    }
-    addjob(fg_pid, st);
   }
+
+  if (sigprocmask(SIG_SETMASK, &prev_all, NULL) < 0)
+    unix_error("sigprocmask set mask error");
 }
 
-// TODO: synchronize signal handling (p770-780).
+// Reaps children and updates the job status to be one of the following:
+// TERMINATED: when child exists normally or terminated by a signal.
+// STOPPED: when child is suspended with SIGTSTP.
+// RUNNING: when child resumes execution.
 void reap_child(int sig) {
-  printf("reap child handler");
-  // int status;
-  // pid_t pid = waitpid(-1, &status, 0);
-  // if (errno != ECHILD) {
-  //   unix_error("waitpid error");
-  // }
+  sigset_t mask_all, prev_all;
+  if (sigfillset(&mask_all) < 0)
+    unix_error("sigfillset error");
+
+  if (sigprocmask(SIG_BLOCK, &mask_all, &prev_all) < 0)
+    unix_error("sigprocmask block error");
+
+  int status;
+  pid_t pid;
+
+  // We want to be informed if children were terminated, stopped or continued
+  // so that we can keep update their status accordingly.
+  while ((pid = waitpid(-1, &status, WNOHANG | WUNTRACED | WCONTINUED)) > 0) {
+    Status st;
+    if (WIFEXITED(status) || WIFSIGNALED(status))
+      st = TERMINATED;
+    else if (WIFSTOPPED(status))
+      st = STOPPED;
+    else // WIFCONTINUED
+      st = RUNNING;
+    printf("[ReapChildHandler] setjobstat %d to %s\n", pid, jstatus_str(st));
+    setjobstat(pid, st);
+  }
+
+  assert(pid == 0 || errno == ECHILD);
+
+  if (sigprocmask(SIG_SETMASK, &prev_all, NULL) < 0)
+    unix_error("sigprocmask set mask error");
 }
 
 pid_t Fork() {
@@ -139,6 +207,12 @@ pid_t Fork() {
 }
 
 int main() {
+  sigset_t mask_one, prev_one;
+  if (sigemptyset(&mask_one) < 0)
+    unix_error("sigempty error");
+  if (sigaddset(&mask_one, SIGCHLD) < 0)
+    unix_error("sigaddset error");
+
   if (signal(SIGINT, forward_signal) == SIG_ERR) {
     unix_error("Install SIGINT handler error");
   }
@@ -157,7 +231,17 @@ int main() {
     if (feof(stdin))
       exit(0);
 
+    // Block SIGCHLD before eval. In the next loop iter when we get to fgets
+    // SIGCHLD handler will get a chance to run. We don't have to block all
+    // signals because we still want to receive SIGINT and SIGTSTP and forward
+    // them to the foreground process.
+    if (sigprocmask(SIG_BLOCK, &mask_one, &prev_one) < 0)
+      unix_error("sigprocmask block error");
+
     eval(cmdline);
+
+    if (sigprocmask(SIG_SETMASK, &prev_one, NULL) < 0)
+      unix_error("sigprocmask set mask error");
   }
 }
 
@@ -187,7 +271,7 @@ void eval(char *cmdline) {
       // stp_pid is the pid of a stopped background process which now
       // needs to resume and run in foreground.
       if (kill(stp_pid, SIGCONT) < 0) {
-        unix_error("Forward signal error");
+        unix_error("Forward SIGCONT error");
       }
       run_fg(stp_pid);
       return;
@@ -208,8 +292,12 @@ void eval(char *cmdline) {
     if ((stp_pid = parse_pid(id_part)) > 0) {
       // stp_pid is the pid of a stopped background process which now
       // needs to resume and run in background.
+      //
+      // If the process already terminated, we should have received SIGCHLD for
+      // it before the current run of eval which will change its status, thus
+      // parse_pid will return -1.
       if (kill(stp_pid, SIGCONT) < 0) {
-        unix_error("Forward signal error");
+        unix_error("Forward SIGCONT error");
       }
       run_bg(stp_pid, "");
       return;
@@ -278,7 +366,6 @@ int parseline(char *buf, char **argv) {
 // 0: it was not build-in command.
 int builtin_command(char **argv) {
   // strcmp return 0 if strings are equal.
-  printf("argv[0] %s\n", argv[0]);
   if (!strcmp(argv[0], "quit"))
     exit(0);
 
@@ -351,7 +438,7 @@ void run_fg(pid_t pid) {
 
   /*
    * Because we explicitly wait for this pid, if another background process
-   * terminates we wont mistakenly reap it here instead of running this child
+   * terminates we won't mistakenly reap it here instead of running this child
    * process to completion.
    */
   int status;
@@ -359,12 +446,35 @@ void run_fg(pid_t pid) {
     unix_error("waitpid error");
 
   if (WIFSIGNALED(status)) {
-    // Because command was run in foreground it doesn't have a job ID.
-    sprintf(sigbuf, "Job [-] %d terminated by signal", pid);
+    int jid;
+    if ((jid = setjobstat(pid, TERMINATED)) != -1)
+      sprintf(sigbuf, "Job [%d] %d terminated by signal", jid, pid);
+    else
+      sprintf(sigbuf, "Job [-] %d terminated by signal", pid);
     psignal(WTERMSIG(status), sigbuf);
   } else if (WIFSTOPPED(status)) {
-    // Because command was run in foreground it doesn't have a job ID.
-    sprintf(sigbuf, "Job [-] %d stopped by signal", pid);
+    fg_pid = 0;
+
+    int jid;
+    if ((jid = addjob(pid, STOPPED)) < 0) {
+      printf("Could not add new job\n");
+      exit(0);
+    }
+
+    // For children terminated by Ctrl+C, clear fg_pid so we avoid sending the
+    // same signal again to a terminated process. Since the child is already
+    // terminated the shell will crash.
+    //
+    // For children stopped by Ctrl+Z, clear fg_pid so we avoid sending SIGTSTP
+    // several times to an already stopped process, and also add them to jobs
+    // list as STOPPED. The shell won't crash but it's not necessary to send
+    // that signal over and over again if the child is already suspended.
+    //
+    // For children that terminate normally also we need to clear fg_pid to
+    // avoid sending signals to a terminated process.
+    fg_pid = 0;
+
+    sprintf(sigbuf, "Job [%d] %d stopped by signal", jid, pid);
     psignal(WSTOPSIG(status), sigbuf);
   }
 }
